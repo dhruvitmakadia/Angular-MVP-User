8ce4c55ce0f48d692b448867dea5f484d99258d6
Dhruvit Makadia <dhruvit.makadia@1rivet.local>
2020-02-17 18:00:10 +0530

    Design Listing

--- src/app/user/user-component/user.component.ts
+++ src/app/user/user-component/user.component.ts
@@ -12,4 +12,23 @@ export class UserComponent implements OnInit {
   ngOnInit() {
   }
 
+  /**
+   * Toggle between showing and hiding the sidebar, and add overlay effect
+   */
+  w3_open() {
+    if (document.getElementById('mySidebar').style.display === 'block') {
+      document.getElementById('mySidebar').style.display = 'none';
+      document.getElementById('myOverlay').style.display = 'none';
+    } else {
+      document.getElementById('mySidebar').style.display = 'block';
+      document.getElementById('myOverlay').style.display = 'block';
+    }
+  }
+  /**
+   * Close the sidebar with the close button
+   */
+  w3_close() {
+    document.getElementById('mySidebar').style.display = 'none';
+    document.getElementById('myOverlay').style.display = 'none';
+  }
 }
--- src/app/user/user-list-container/user-list-presentation/user-list-presentation.ts
+++ src/app/user/user-list-container/user-list-presentation/user-list-presentation.ts
@@ -12,6 +12,13 @@ export class UserListPresentation implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const input = document.getElementById('search-input');
+    const searchBtn = document.getElementById('search-btn');
+    const expand = () => {
+      searchBtn.classList.toggle('close');
+      input.classList.toggle('square');
+    };
+    searchBtn.addEventListener('click', expand);
   }
 
 }
